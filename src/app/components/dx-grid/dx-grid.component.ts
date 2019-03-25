cacdafa6c7fa54e44597743bd27220c409c0d482
Idan <IdanArgaman@gmail.com>
2019-03-25 10:07:11 +0200

    Gtid Component (DX)

---
+++ src/app/components/dx-grid/dx-grid.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dx-grid',
+  templateUrl: './dx-grid.component.html',
+  styleUrls: ['./dx-grid.component.css']
+})
+export class DxGridComponent implements OnInit {
+
+  customers = [{
+    'ID': 1,
+    'CompanyName': 'Super Mart of the West',
+    'Address': '702 SW 8th Street',
+    'City': 'Bentonville',
+    'State': 'Arkansas',
+    'Zipcode': 72716,
+    'Phone': '(800) 555-2797',
+    'Fax': '(800) 555-2171',
+    'Website': 'http://www.nowebsitesupermart.com'
+  }, {
+    'ID': 2,
+    'CompanyName': 'Electronics Depot',
+    'Address': '2455 Paces Ferry Road NW',
+    'City': 'Atlanta',
+    'State': 'Georgia',
+    'Zipcode': 30339,
+    'Phone': '(800) 595-3232',
+    'Fax': '(800) 595-3231',
+    'Website': 'http://www.nowebsitedepot.com'
+  }];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
